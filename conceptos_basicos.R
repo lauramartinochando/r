3b0335cd2165169083096ff7aaf68f207bbda1b3
lauramartinochando <ochando.lauramartin@gmail.com>
2023-12-11 11:29:09 +0100

    Create conceptos_basicos.R

---
+++ conceptos_basicos.R
@@ -0,0 +1,43 @@
+#ejer 1: Define una variable llamada numero con el valor 10 y otra llamada
+#nombre con tu nombre.
+numero <- 10
+nombre <- "Laura"
+
+#ejer 2: Utiliza las funciones class e is.numeric para determinar el tipo de
+#dato de numero.
+class(numero)
+is.numeric(numero)
+
+#ejer 3: Realiza una operación aritmética que sume numero y el doble de
+#numero.
+
+#ejer 4: Crea un vector llamado edades con las edades de tres personas y
+#una lista llamada informacion con el nombre y la edad de una persona.
+edades <- c(13, 80, 63)
+informacion <- list(nombre="Raul", edad=29)
+
+#ejer 5: Verifica si nombre es de tipo caracter y si es_numerico es de tipo
+#lógico.
+is.character(nombre)
+is.logical(edad)
+
+#ejer 6: Crea una variable llamada mayor_de_edad que sea TRUE si la edad
+#de la primera persona en edades es mayor o igual a 18.
+mayor_de_edad <-
+
+#ejer 7: Utiliza el operador %in% para verificar si el valor 30 está presente
+#en el vector edades .
+
+#ejer 8:  Compara si el doble de numero es mayor que edades[3] .
+
+#ejer 9: Define dos variables lógicas, condicion1 y condicion2 , ambas con
+#valor TRUE . Comprueba si ambas condiciones son verdaderas.
+
+#ejer 10: Define una variable lógica, verdadero , con valor TRUE . Comprueba
+#que su valor NO sea verdadero.
+
+
+
+
+
+
